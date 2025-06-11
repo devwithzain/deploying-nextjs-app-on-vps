511098547b9d3aed2694424150431bf70b7814b0
devwithzain <zainsoftwear11@gmail.com>
2025-06-11 23:33:53 +0500

    modified

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
 						Save and see your changes instantly.
 					</li>
 					<li className="tracking-[-.01em]">
-						First Change on vps server. By devwithzain.
+						First Change on vps server. By umairkhan.
 					</li>
 				</ol>
 
